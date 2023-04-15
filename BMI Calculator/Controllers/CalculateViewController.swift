fc7463c6e060f5050fb389c524c334ad4bc45403
kheladzedev <edward.developer@icloud.com>
2023-04-15 17:10:29 +0300

    first commit

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CalculateViewController.swift
+//  BMI Calculator
+//
+//  Created by Edward Kheladze on 15.04.2023.
+//
+
+import Foundation
---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Edward Kheladze on 15.04.2023.
+//
+
+import Foundation
